4284fd4f908c1452ec96192ea983aa413274cd7b
Amadou Sene <amadousene64@gmail.com>
2025-08-16 11:29:05 +0000

    First commit

---
+++ src/test/java/sn/minfinances/EvalsecurrityApplicationTests.java
@@ -0,0 +1,16 @@
+package sn.minfinances;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class EvalsecurrityApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
